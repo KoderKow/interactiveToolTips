de55903c9ecf149fe1fd2bb1341a283cb87d5fda
Kyle Harris <koderkow@gmail.com>
2020-07-26 11:43:24 -0500

    Fill in about, add GitHub link

--- inst/app/www/script.js
+++ inst/app/www/script.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
   let github_icon = "body > nav > div > ul > li:nth-child(4) > a";
   $(github_icon).attr({
-    href: "https://github.com/",
+    href: "https://github.com/KoderKow/interactiveToolTips",
     target: "_blank"
     });
   $(github_icon).removeAttr("data-toggle");
